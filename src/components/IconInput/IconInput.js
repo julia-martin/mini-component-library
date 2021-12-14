36b8888d798b0e9f2cbba337f67673a231b40524
Julia <juliadmartin720@gmail.com>
2021-12-13 19:02:58 -0800

    Create styles for Icon Input component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,13 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+
+
+const ICON_MAP = {
+  small: '12px',
+  large: '20px'
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +20,55 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const SIZE_MAP = {
+    small: {
+      '--borderWidth': '1px',
+      '--fontSize': '14px',
+      '--width': width + 'px'
+    },
+    large: {
+      '--borderWidth': '2px',
+      '--fontSize': '18px',
+      '--width': width + 'px'
+    }
+  };
+
+  return (
+    <Wrapper style={SIZE_MAP[size]}>
+      <label for="search" style={{ height: 0, visibility: 'hidden' }}></label>
+      <Icon id={icon} size={ICON_MAP[size]} name="search" />
+      <Input placeholder={placeholder} />
+    </Wrapper>
+  )
 };
+
+const Wrapper = styled.div`
+  border-top: none;
+  border-right: none;
+  border-left: none;
+  border-bottom-style: solid;
+  border-bottom-width: var(--borderWidth);
+  font-size: var(--fontSize);
+  width: var(--width);
+`;
+
+const Input = styled.input`
+  border: none;
+  color: ${COLORS.gray700};
+  font-weight: bold;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: normal;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &:focus {
+    outline-offset: 4px;
+  }
+`;
 
 export default IconInput;
