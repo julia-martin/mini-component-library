340ba2fa65a7fec208effabea36bec9762a3cf2c
Julia <juliadmartin720@gmail.com>
2021-12-13 19:02:46 -0800

    Create styles for Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,43 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <CustomSelect value={value} onChange={onChange}>
+        {children}
+      </CustomSelect>
+      <IconWrapper>
+        <Icon id='chevron-down' size='12' />
+      </IconWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  float: left;
+  height: 43px;
+  position: relative;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  right: 18px;
+  top: 0px;
+  bottom: 0px;
+  padding: 18px 0px 12px;
+`;
+
+const CustomSelect = styled.select`
+  appearance: none;
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  height: 100%;
+  width: 100%;
+  padding: 12px 52px 12px 16px;
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
